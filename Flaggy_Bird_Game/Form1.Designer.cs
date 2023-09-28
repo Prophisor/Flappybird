878cc53b977ef57e24e4977aee2bb22e115b04e1
Siyamend Abde <siyamend.abde@outlook.de>
2023-09-28 16:23:01 +0200

    first commit

---
+++ Flaggy_Bird_Game/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace Flaggy_Bird_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.scoreText = new System.Windows.Forms.Label();
+            this.gameTime = new System.Windows.Forms.Timer(this.components);
+            this.flaggyBird = new System.Windows.Forms.PictureBox();
+            this.pipeTop = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.pipeButtom = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.flaggyBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeButtom)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.Font = new System.Drawing.Font("Verdana", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreText.Location = new System.Drawing.Point(9, 7);
+            this.scoreText.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(139, 32);
+            this.scoreText.TabIndex = 1;
+            this.scoreText.Text = "Score: 0";
+            this.scoreText.Click += new System.EventHandler(this.scoreText_Click);
+            // 
+            // gameTime
+            // 
+            this.gameTime.Enabled = true;
+            this.gameTime.Interval = 20;
+            this.gameTime.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // flaggyBird
+            // 
+            this.flaggyBird.Image = global::Flaggy_Bird_Game.Properties.Resources.bird;
+            this.flaggyBird.Location = new System.Drawing.Point(58, 42);
+            this.flaggyBird.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.flaggyBird.Name = "flaggyBird";
+            this.flaggyBird.Size = new System.Drawing.Size(45, 49);
+            this.flaggyBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.flaggyBird.TabIndex = 0;
+            this.flaggyBird.TabStop = false;
+            this.flaggyBird.Click += new System.EventHandler(this.flaggyBird_Click);
+            // 
+            // pipeTop
+            // 
+            this.pipeTop.Image = global::Flaggy_Bird_Game.Properties.Resources.pipedown;
+            this.pipeTop.Location = new System.Drawing.Point(926, -22);
+            this.pipeTop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pipeTop.Name = "pipeTop";
+            this.pipeTop.Size = new System.Drawing.Size(97, 189);
+            this.pipeTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop.TabIndex = 0;
+            this.pipeTop.TabStop = false;
+            this.pipeTop.Click += new System.EventHandler(this.pipeTop_Click);
+            // 
+            // ground
+            // 
+            this.ground.Image = global::Flaggy_Bird_Game.Properties.Resources.ground;
+            this.ground.Location = new System.Drawing.Point(-31, 498);
+            this.ground.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(1075, 133);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 0;
+            this.ground.TabStop = false;
+            // 
+            // pipeButtom
+            // 
+            this.pipeButtom.Image = global::Flaggy_Bird_Game.Properties.Resources.pipe;
+            this.pipeButtom.Location = new System.Drawing.Point(694, 340);
+            this.pipeButtom.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pipeButtom.Name = "pipeButtom";
+            this.pipeButtom.Size = new System.Drawing.Size(97, 175);
+            this.pipeButtom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeButtom.TabIndex = 0;
+            this.pipeButtom.TabStop = false;
+            this.pipeButtom.Click += new System.EventHandler(this.pipeButtom_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 531);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Restart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 642);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flaggyBird);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.pipeTop);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.pipeButtom);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gamekeyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gamekeyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.flaggyBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeButtom)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox flaggyBird;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox pipeButtom;
+        private System.Windows.Forms.PictureBox pipeTop;
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Timer gameTime;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Flaggy_Bird_Game/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flaggy_Bird_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+
+            InitializeComponent();
+        }
+        int pipeSpeed = 8; // sppd in left direction
+        int birdSpeed = 14; // down and up 
+        int score = 0;
+        private void pipeTop_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            GameTimer();
+        }
+
+        private void GameTimer()
+        {
+            flaggyBird.Top += birdSpeed;
+            pipeButtom.Left -= pipeSpeed;
+            pipeTop.Left -= pipeSpeed;
+            scoreText.Text = "Score: " + score;
+            if (gameTime.Enabled == true)
+            {
+                button1.Enabled = false;
+            }
+            if (pipeButtom.Left < -150)
+            {
+                pipeButtom.Left = 800;
+                score++;
+            }
+            if (pipeTop.Left < -150)
+            {
+                pipeTop.Left = 900;
+                score++;
+            }
+            if (flaggyBird.Bounds.IntersectsWith(pipeButtom.Bounds) ||
+                flaggyBird.Bounds.IntersectsWith(pipeTop.Bounds) ||
+                flaggyBird.Bounds.IntersectsWith(ground.Bounds) ||
+                flaggyBird.Top < -25)
+            {
+                endGame();
+            }
+            if (score == 10)
+            {
+                pipeSpeed = 10;
+            }
+            if (score == 20)
+            {
+                pipeSpeed = 13;
+            }
+            if (score == 30)
+            {
+                pipeSpeed = 15;
+            }
+            if (score == 60)
+            {
+                pipeSpeed = 17;
+            }
+        }
+
+        private void gamekeyisdown(object sender, KeyEventArgs e)
+        {
+            GameKeyisdown(e);
+        }
+
+        private void GameKeyisdown(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                birdSpeed = -15;
+            }
+        }
+
+        private void gamekeyisup(object sender, KeyEventArgs e)
+        {
+            GameKeyisup(e);
+        }
+
+        private void GameKeyisup(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                birdSpeed = +15;
+            }
+        }
+
+        private void flaggyBird_Click(object sender, EventArgs e)
+        {
+
+
+        }
+        private void endGame()
+        {
+            gameTime.Stop();
+            scoreText.Text += " Game over!!";
+            scoreText.BackColor = Color.CadetBlue;
+            if (gameTime.Enabled == false)
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private void scoreText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gameTime.Enabled = true;
+            flaggyBird.Top = birdSpeed;
+           pipeButtom.Left = 950;
+            pipeTop.Left = 500;
+            score = 0;
+        }
+
+        private void pipeButtom_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
